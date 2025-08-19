056a8e5a32b5cab6d4530d95879fbfd07fbb4fc6
Abubakaryounis97 <Abubakaryounis97@gmail.com>
2025-08-19 16:36:36 -0400

    working on animal class

--- src/main/java/com/zipcodewilmington/froilansfarm/Base_Abstract_Classes/Animal.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Base_Abstract_Classes/Animal.java
@@ -1,5 +1,29 @@
 package com.zipcodewilmington.froilansfarm.Base_Abstract_Classes;
 
-public class Animal {
-    
+import com.zipcodewilmington.froilansfarm.Interfaces.Eater;
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+import com.zipcodewilmington.froilansfarm.Interfaces.NoiseMaker;
+
+public abstract  class Animal implements NoiseMaker, Eater {
+
+    String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void makeNoise() {
+    }
+
+    @Override
+    public void eat(Edible object) {
+    }
+
+   
+      
 }
