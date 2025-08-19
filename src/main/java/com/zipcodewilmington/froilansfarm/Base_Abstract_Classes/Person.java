d2b44c666ae231ce3f8113fb01205a5895b4f2a0
danishahsancs <danishahsancs@gmail.com>
2025-08-19 16:36:08 -0400

    made person base abstract class

--- src/main/java/com/zipcodewilmington/froilansfarm/Base_Abstract_Classes/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Base_Abstract_Classes/Person.java
@@ -1,5 +1,37 @@
 package com.zipcodewilmington.froilansfarm.Base_Abstract_Classes;
 
-public class Person {
-    
+import com.zipcodewilmington.froilansfarm.Interfaces.Eater;
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+import com.zipcodewilmington.froilansfarm.Interfaces.NoiseMaker;
+import com.zipcodewilmington.froilansfarm.Interfaces.Rideable;
+import com.zipcodewilmington.froilansfarm.Interfaces.Rider;
+
+public abstract class Person implements Rider, Eater, NoiseMaker {
+
+    String name;
+
+    @Override
+    public void makeNoise() {
+    }
+
+    @Override
+    public void eat(Edible object) {
+    }
+
+    @Override
+    public void mount(Rideable rideable) {
+    }
+
+    @Override
+    public void dismount(Rideable rideable) {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
