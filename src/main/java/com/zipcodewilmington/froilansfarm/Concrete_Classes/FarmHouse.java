ee30e2d1174a9d96c82bd8c16bc93447f4c0988b
danishahsancs <danishahsancs@gmail.com>
2025-08-20 14:18:48 -0400

    person pilot and shelter eand its extensions

--- src/main/java/com/zipcodewilmington/froilansfarm/Concrete_Classes/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Concrete_Classes/FarmHouse.java
@@ -1,5 +1,13 @@
 package com.zipcodewilmington.froilansfarm.Concrete_Classes;
 
-public class FarmHouse {
+import com.zipcodewilmington.froilansfarm.Base_Abstract_Classes.Person;
+import com.zipcodewilmington.froilansfarm.Base_Abstract_Classes.Shelter;
+
+public class FarmHouse extends Shelter<Person>{
+
+    public FarmHouse() {
+        super("farmhouse for people");
+        
+    }
     
 }
