5d4273ccebfcc38d9d145a30bc082da3ebf904bd
Abubakaryounis97 <Abubakaryounis97@gmail.com>
2025-08-22 08:44:46 -0400

    Tomato plant test class

--- src/test/java/com/zipcodewilmington/froilansfarm/Concrete_Classes/ChickenTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Concrete_Classes/ChickenTest.java
@@ -1,19 +1,56 @@
 package com.zipcodewilmington.froilansfarm.Concrete_Classes;
 
-import com.zipcodewilmington.froilansfarm.Base_Abstract_Classes.Animal;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+
 public class ChickenTest {
- @Test
-    public void testChickenIsAnimal() {
+ 
+    @Test
+    public void testMakeNoise() {
+        
         Chicken chicken = new Chicken();
-        assertTrue(chicken instanceof Animal, "Chicken should be a subclass of Animal");
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        chicken.makeNoise();
+
+        assertEquals("Cluck\n", outContent.toString());
     }
-    
+
+    @Test
+    public void testEat() {
+        Chicken chicken = new Chicken();
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Edible food = new Edible() {};
+
+        chicken.eat(food);
+
+        assertEquals("Chicken is pecking at the food.\n", outContent.toString());
+    }
+
+     @Test
+    public void testYieldBeforeFertilization() {
+        Chicken chicken = new Chicken();
+        assertNull(chicken.yield(), "Yield should be null if not fertilized");
+    }
+
+    @Test
+    public void testHasBeenFertilizedFlag() {
+        
+        Chicken chicken = new Chicken();
+       
+        chicken.fertilize();
+        assertTrue(chicken.hasBeenFertilized(), "Should be fertilized after calling fertilize()");
+    }
 
 }
